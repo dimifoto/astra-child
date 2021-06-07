dce5ffa1e6daa7ad9da929caead762ab38739840
dimifoto <79830992+dimifoto@users.noreply.github.com>
2021-06-07 10:17:31 +0300

    loading custom.css

--- functions.php
+++ functions.php
@@ -11,3 +11,9 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
+
+function load_custom_files() {
+	wp_register_style( 'custom_css', get_stylesheet_directory_uri(). '/assets/css/custom.css');
+	wp_enqueue_style( 'custom_css' );
+}
+add_action ('wp_enqueue_scripts', 'load_custom_files', 10 );
