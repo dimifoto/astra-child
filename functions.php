7444a92ab1876fe961e3f1033e138d1873219334
dimifoto <79830992+dimifoto@users.noreply.github.com>
2021-06-07 09:58:47 +0300

    functions.php

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Astra functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
